65b4ff83cecf5dc758c8a25e3cf27ef74dd22fd0
Pradhumna Panholi <user148657@ny009.local>
2019-02-09 03:50:44 -0500

    added  assets and data, MasterViewController modified to load data as expected

---
+++ Explorer/Data.swift
@@ -0,0 +1,11 @@
+//
+//  Data.swift
+//  Explorer
+//
+//  Created by user148657 on 2/9/19.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+import Foundation
+
+//Name for places//
+let places = ["amsterdam","vegas","vancouver","paris","tokyo","corsica"]
