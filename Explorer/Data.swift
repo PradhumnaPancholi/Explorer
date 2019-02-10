c89d90b6cf6ae6fcaf19829724866016bd3d3501
Pradhumna Panholi <user148657@ny009.local>
2019-02-10 17:25:47 -0500

    data for DetailsView

--- Explorer/Data.swift
+++ Explorer/Data.swift
@@ -5,9 +5,23 @@
 //  Created by user148657 on 2/9/19.
 //  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
 //
-import Foundation
+import UIKit
 
 //Name of places//
 let places = ["amsterdam","vegas","vancouver","paris","tokyo","corsica"]
 
-//for imagess//
+//images//
+let amsterdam = UIImage(named: "amsterdam")
+let corsica = UIImage(named: "corsica")
+let tokyo = UIImage(named: "tokyo")
+let paris = UIImage(named: "paris")
+let vegas = UIImage(named: "vegas")
+let vancouver = UIImage(named: "vancouver")
+
+//array of images//
+let imageArray = [amsterdam, corsica, tokyo, paris, vegas, vancouver]
+
+
+
+
+
